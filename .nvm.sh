51da3639ae56d5f00174ae08c2bf2a31eb88f02d
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2018-11-03 00:07:03 -0300

    Add stuff

---
+++ .nvm.sh
@@ -0,0 +1,3 @@
+export NVM_DIR="$HOME/.nvm"
+105 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+106 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
